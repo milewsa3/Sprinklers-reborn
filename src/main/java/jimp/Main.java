00473052058293d67bffbc8b6553551a35b6ae05
Adrian Milewski <milewsa3@ee.pw.edu.pl>
2020-05-20 21:45:54 +0200

    Init commit

---
+++ src/main/java/jimp/Main.java
@@ -0,0 +1,17 @@
+package jimp;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Sprinklers");
+        primaryStage.show();
+    }
+}
